79d4fb57ad05222b0a1b35797004a5a39e693a36
Charissa Law <charissalaw@Charissas-MacBook-Pro.local>
2016-08-05 16:35:41 -0400

    uploading directories and files to s3

--- upload_script.py
+++ upload_script.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 
 s3client = boto3.client('s3')
 
@@ -9,5 +10,34 @@ s3resource = boto3.resource('s3')
 data = open('message.txt', 'r')
 s3resource.Bucket('test-charissa').put_object(Key='word4-word5-word6', Body=data)
 
-# for bucket in s3resource.buckets.all():
-    # print(bucket.name)
+path = "../boop-bawp-beep"
+bucketname = "test-charissa"
+
+def uploadDirectory(path, bucketname):
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            directory_name = os.path.join(root, dir) + "/"
+            directory_name = directory_name.replace("../", "")
+            s3client.put_object(
+                Bucket='test-charissa',
+                Body='',
+                Key=directory_name
+            )
+        for file in files:
+            filename = os.path.join(file)
+            file_with_path = os.path.join(root, file)
+
+            data = open(file_with_path, 'r')
+            new_directory = file_with_path.replace("../", "")
+            s3client.put_object(
+                Bucket='test-charissa',
+                Body=data,
+                Key=new_directory
+            )
+
+
+# find names of directories in current path
+s3client.list_objects(Bucket='test-charissa')
+# find names of directories on s3
+# if name in current path does not exist on s3, then upload that directory
+uploadDirectory(path, bucketname)
